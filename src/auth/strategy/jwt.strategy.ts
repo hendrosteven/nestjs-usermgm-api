2063094f081108f2be5ab99fde0bad17a8928fbd
Hendro Steven <hendro.steven@gmail.com>
2022-06-30 06:51:17 +0700

    add profile route

---
+++ src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,31 @@
+import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { PassportStrategy } from "@nestjs/passport";
+import { ExtractJwt, Strategy } from "passport-jwt";
+import { PrismaService } from '../../prisma/prisma.service';
+
+/**
+ * To validate jwt token
+ */
+
+@Injectable({})
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt'){
+
+    constructor(config: ConfigService, private prisma: PrismaService){
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            secretOrKey: config.get('JWT_SECRET'),  
+        })
+    }
+
+    // validate to token and append to the user request object 
+    async validate(payload: {sub: string, email: string}){
+        const user = await this.prisma.user.findUnique({
+            where:{
+                id:payload.sub
+            }
+        });
+        delete user.hash;
+        return user;
+    }
+}
---
+++ src/user/user.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { User } from '@prisma/client';
+import { GetUser } from '../auth/decorator';
+import { JwtGuard } from '../auth/guard';
+
+@UseGuards(JwtGuard)
+@Controller('users')
+export class UserController {
+
+    @Get('profile')
+    profile(@GetUser() user: User){
+        return user;
+    }
+
+}

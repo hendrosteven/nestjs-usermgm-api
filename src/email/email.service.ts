71d71fe8771efc6ef67f21ae9bb5be848b1b91bf
Hendro Steven <hendro.steven@gmail.com>
2022-07-07 22:48:06 +0700

    update email link

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -12,7 +12,7 @@ export class EmailService {
         const hostname = process.env.MAILGUN_SMTP_SERVER || this.config.get('HOSTNAME');        
         const username = process.env.MAILGUN_SMTP_LOGIN || this.config.get('USERNAME');
         const password = process.env.MAILGUN_SMTP_PASSWORD || this.config.get('PASSWORD');        
-        const domain = this.config.get('DOMAIN') || 'user-app-api.herokuapp.com';
+        const domain = 'https://user-app-api.herokuapp.com' || this.config.get('DOMAIN') ;
         const path = domain +"/auth/verify/"+userId;
 
         const transporter = nodemailer.createTransport({
